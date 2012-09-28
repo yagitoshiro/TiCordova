27e68fefc80b29059c8ac07bb4aca53f3c38ec37
Toshiro Yagi <yagitoshiro@gmail.com>
2012-09-28 21:36:52 +0900

    init

---
+++ Classes/OrgSelfkleptomaniacTiModTicordovaWebView.m
@@ -0,0 +1,81 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2009-2012 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
+#import "OrgSelfkleptomaniacTiModTicordovaWebView.h"
+#import "TiUtils.h"
+
+@implementation OrgSelfkleptomaniacTiModTicordovaWebView
+
+#pragma mark LIFECYCLE
+-(void)willMoveToSuperview:(UIView *)newSuperview
+{
+	NSLog(@"[VIEW LIFECYCLE EVENT] willMoveToSuperview");
+}
+
+-(void)initializeState
+{
+	// This method is called right after allocating the view and
+	// is useful for initializing anything specific to the view
+	
+	[super initializeState];
+	
+	NSLog(@"[VIEW LIFECYCLE EVENT] initializeState");
+}
+
+-(void)configurationSet
+{
+	// This method is called right after all view properties have
+	// been initialized from the view proxy. If the view is dependent
+	// upon any properties being initialized then this is the method
+	// to implement the dependent functionality.
+	
+	[super configurationSet];
+	
+	NSLog(@"[VIEW LIFECYCLE EVENT] configurationSet");
+}
+
+#pragma mark IMPORTANT NOTE
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    BOOL * parentResult = [super webView:webView shouldStartLoadWithRequest:request navigationType:navigationType];
+    
+    if(!parentResult){
+        return NO;
+    }else{
+        NSURL * newUrl = [request URL];
+        NSString * scheme = [[newUrl scheme] lowercaseString];
+        NSString * path = [newUrl path];
+        if([path hasPrefix:@"/"]){
+            path = [path substringWithRange:NSMakeRange(1, path.length - 1)];
+        }
+        if ([scheme isEqualToString:@"fireevent"])
+        {
+            NSString * eventName = [newUrl host];
+            NSArray * paths = [path componentsSeparatedByString:@"/"];
+            if ([self.proxy _hasListeners:eventName]){
+                DebugLog(@"[DEBUG] fire: %@",eventName);
+                NSDictionary * event = [NSDictionary dictionaryWithObjectsAndKeys:path, @"path", eventName, @"event", nil];
+                [self.proxy fireEvent:eventName withObject:event];
+            }
+
+            return NO;
+        }
+    }
+    return YES;
+    
+}
+
+#pragma mark Cleanup
+-(void)dealloc
+{
+	NSLog(@"[VIEW LIFECYCLE EVENT] dealloc");
+	
+	[super dealloc];
+}
+
+@end
---
+++ example/app.js
@@ -0,0 +1,48 @@
+// This is a test harness for your module
+// You should do something interesting in this harness 
+// to test out the module and to provide instructions 
+// to users on how to use it by example.
+
+
+// open a single window
+var win = Ti.UI.createWindow({
+	backgroundColor:'white'
+});
+var label = Ti.UI.createLabel();
+win.add(label);
+win.open();
+
+// TODO: write your module tests here
+var ticordova = require('org.selfkleptomaniac.ti.mod.ticordova');
+Ti.API.info("module is => " + ticordova);
+
+label.text = ticordova.example();
+
+Ti.API.info("module exampleProp is => " + ticordova.exampleProp);
+ticordova.exampleProp = "This is a test value";
+
+if (Ti.Platform.name == "android") {
+	var proxy = ticordova.createExample({
+		message: "Creating an example Proxy",
+		backgroundColor: "red",
+		width: 100,
+		height: 100,
+		top: 100,
+		left: 150
+	});
+
+	proxy.printMessage("Hello world!");
+	proxy.message = "Hi world!.  It's me again.";
+	proxy.printMessage("Hello world!");
+	win.add(proxy);
+}
+
+var webview = ticordova.createWebView({url:'http://dev.voidoid.com/webview', top:0, height:Ti.UI.FILL, width:Ti.UI.FILL});
+
+webview.addEventListener('my_event', function(e){
+  Ti.API.info(e);
+  alert(e);
+});
+
+win.add(webview);
+
